dd6b3c77dac22ea556c14855b1c266d2a90af9dc
Oleg <tsybaoleg@gmail.com>
2023-08-28 14:32:01 +0300

    first commit

---
+++ src/components/app/App.js
@@ -0,0 +1,40 @@
+import { Component } from "react";
+import AppHeader from "../appHeader/AppHeader";
+import RandomChar from "../randomChar/RandomChar";
+import CharList from "../charList/CharList";
+import CharInfo from "../charInfo/CharInfo";
+
+import decoration from '../../resources/img/vision.png';
+
+class App extends Component {
+
+    state = {
+        showRandomChar : true
+    }
+
+    toggleRenderChar =()=> {
+        this.setState((state)=>{
+            return {showRandomChar:!state.showRandomChar}
+        })
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    {this.state.showRandomChar ?  <RandomChar/>:null }
+                    <button onClick={this.toggleRenderChar}>Click me</button>
+                    <div className="char__content">
+                        <CharList/>
+                        <CharInfo/>
+                    </div>
+                    <img className="bg-decoration" src={decoration} alt="vision"/>
+                </main>
+            </div>
+        )
+    }
+    
+}
+
+export default App;
---
+++ src/services/MarvelService.js
@@ -0,0 +1,42 @@
+
+
+   class MarvelService {
+
+      _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+      _apiKey = 'apikey=b99ea5f460c68929f9ff4baec562a60b';
+
+   getResourse = async (url)=> {
+      let res =  await fetch(url);//
+
+      if(!res.ok){
+         throw new Error(`Could not fetch ${url}, status ${res.status}`)
+      }
+      return await res.json()
+   }
+
+   getAllCharacters =async()=>{
+      const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+      return res.data.results.map(this._transformCharacter)
+   }
+
+   getCharacter = async (id)=>{
+      const res = await this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`);
+      return this._transformCharacter(res.data.results[0]);
+   }
+
+   _transformCharacter = (char) =>{
+
+      return {
+         name:char.name,
+         description:char.description ? `${char.description.slice(0,250)+ '......'}` :'Character has no description',
+         thumbnail:char.thumbnail.path + '.' +char.thumbnail.extension,
+         homepage:char.urls[0].url,
+         wiki:char.urls[1].url
+      }
+
+   }
+
+} 
+
+
+export default MarvelService;
